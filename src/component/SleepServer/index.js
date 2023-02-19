a7fb473c76678dc04e23d8b16effc2ecffcbed30
boboiazumi <untukherokudeploy@gmail.com>
2023-02-19 08:45:10 +0700

    New Component

---
+++ src/component/SleepServer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class SleepServer extends React.Component{
+    render(){
+        return (
+            <>
+                <img src="/img/sleep.gif" className="mx-0 rounded-md w-screen 2xl:w-screen" alt="NotFound"></img>
+            </>
+        )
+    }
+}
+
+export default SleepServer
