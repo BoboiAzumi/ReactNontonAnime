2a34e177ca986f2bbf8fa3c59310685c7e3a6918
boboiazumi <untukherokudeploy@gmail.com>
2023-03-05 08:55:38 +0700

    Fix

--- src/ui/Settings/index.js
+++ src/ui/Settings/index.js
@@ -73,7 +73,7 @@ class Settings extends React.Component{
                         <p className="text-white text-md">API Server</p>
                         <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={localStorage.getItem("server")} onChange={this.serverSelected.bind(this)}>
                             <option value="https://nontonanime.thedark38.repl.co">https://nontonanime.thedark38.repl.co (Python)</option>
-                            <option value="https://nontonanimejsserver1.cyclic.app" >https://nontonanimejsserver1.cyclic.app (NodeJS)</option>
+                            <option value="https://nontonanimeservernodejs1.cyclic.app" >https://nontonanimeservernodejs1.cyclic.app (NodeJS)</option>
                             <option value="http://localhost:5000" >http://localhost:5000 (Development)</option>
                         </select>
                         <p className="text-white text-md">Interface</p>
